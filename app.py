bfd3eae4bcbaa2c7256ea61865aa9f9edfd4fef5
emmamitm <emmamit.co@gmail.com>
2024-10-25 09:01:39 -0400

    Added useer population to database

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pomodoro.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Database Model
+class Pomodoro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50), nullable=False)
+    user_email = db.Column(db.String(120), nullable=False)
+    work_time = db.Column(db.Integer, nullable=False)
+    break_time = db.Column(db.Integer, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# API route to start the Pomodoro timer
 @app.route('/start_pomodoro', methods=['POST'])
 def start_pomodoro():
     data = request.json
-    work_time = data.get('workTime', 25)  # Default work time is 25 minutes
-    break_time = data.get('breakTime', 5)  # Default break time is 5 minutes
+    user_name = data.get('userName')
+    user_email = data.get('userEmail')
+    work_time = data.get('workTime', 25)
+    break_time = data.get('breakTime', 5)
 
-    # Log the values to show on the server side (you can expand this to store the data)
-    print(f"Work Time: {work_time} minutes, Break Time: {break_time} minutes")
+    # Save to database
+    pomodoro = Pomodoro(user_name=user_name, user_email=user_email, work_time=work_time, break_time=break_time)
+    db.session.add(pomodoro)
+    db.session.commit()
 
     return jsonify({"message": "Pomodoro started with custom work and break times"})
 
